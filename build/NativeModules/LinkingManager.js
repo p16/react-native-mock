b4c4221c9e5814f03d64431922d8bb30c1294541
Filippo De Santis <filippo.desantis@gmail.com>
2016-04-21 12:01:25 +0200

    fixes post install from repo

---
+++ build/NativeModules/LinkingManager.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var _test = function _test(url) {
+  return true;
+};
+var LinkingManger = {
+  openURL: function () {
+    function openURL(url) {
+      return Promise.resolve(true);
+    }
+
+    return openURL;
+  }(),
+  canOpenURL: function () {
+    function canOpenURL(url) {
+      return Promise.resolve(_test(url));
+    }
+
+    return canOpenURL;
+  }(),
+  __setCanOpenURLTest: function () {
+    function __setCanOpenURLTest(test) {
+      _test = test;
+    }
+
+    return __setCanOpenURLTest;
+  }()
+};
+
+module.exports = LinkingManger;
